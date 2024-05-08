58b3e659d884c9a4b76a02eb4217eb061f31fdb5
Jorge Thomas <jorgethomasm@hotmail.com>
2024-05-08 15:54:18 +0200

    first commit

---
+++ init.R
@@ -0,0 +1,22 @@
+# ======================== e-US-demand-forecast =========================
+
+# https://jorgethomasm.github.io/
+# May 2024
+# J. Thomas
+
+
+# ========================== Load Dependencies ==========================
+
+library(EIAapi)
+library(tidyverse)
+library(plotly)
+
+
+# ======================== Load Helper Functions ========================
+
+source("./tools/jthomfuncs.r")
+theme_set(jthomggtheme)
+
+
+# ======================== Initialise Variables ========================
+
---
+++ main.R
@@ -0,0 +1,3 @@
+# Init vars
+
+source("./init.R")
